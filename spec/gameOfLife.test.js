2a280628fb566784cc14045c101ec26534a65fe4
Anna Cavalla <Anna.Cavalla@gmail.com>
2021-04-28 22:59:57 +0100

    Converted ruby code to js

--- spec/gameOfLife.test.js
+++ spec/gameOfLife.test.js
@@ -1,104 +1,169 @@
-'use strict';
+// 'use strict';
+const { test, expect } = require('@jest/globals');
+const Board = require('../src/gameOfLife.js');
 
-const sum = require('../src/gameOfLife.js');
+describe('Board class', function() {
 
-describe('Thermostat', function() {
+  let board = new Board
 
-  // var thermostat;
-  test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
-  });
-
-  test('two plus two is four', () => {
-    expect(sum(2, 2)).not.toBe(5);
-  });
-
-  test('object assignment', () => {
-    const data = {one: 1};
-    data['two'] = 2;
-    expect(data).toEqual({one: 1, two: 2});
-  });
-  beforeEach(function() {
-    thermostat = new Thermostat();
-  });
-
-  describe('initialization', function(){
-    it('starts at temperature 20', function() {
-      expect(thermostat.currentTemp).toBe(20);
+  describe('.alive', function(){
+    it('saves live cells in an array', function() {
+      board.alive([1,2])
+      expect(board.live[0]).toMatchObject([1,2]);
     });
   });
 
-  describe('up',function(){
-    it('can increase the temperature', function(){
-      thermostat.up();
-      expect(thermostat.currentTemp).toBe(21);
+  describe('.dead', function(){
+    it('removes cells from an array', function() {
+      board.alive([1,2])
+      board.dead([1,2])
+      expect(board.live).toMatchObject([]);
     });
   });
 
-  describe('down',function(){
-    it('can decrease the temperature', function(){
-      thermostat.down();
-      expect(thermostat.currentTemp).toBe(19);
+  describe('.livingConditions', function(){
+    h = [{ loc: [ 2, 1 ], tally: 2, status: 1 },
+         { loc: [ 2, 2 ], tally: 3, status: 0 },
+         { loc: [ 0, 0 ], tally: 1, status: 1 }]
+    it('returns true when living cell has two neighbours', function() {
+      expect(board.livingConditions(h[0])).toBe(true);
+    });
+
+    it('returns true when dead cell has three live neighbours', function() {
+      expect(board.livingConditions(h[1])).toBe(true);
+    });
+
+    it('returns false when live cell has one live neighbour', function() {
+      expect(board.livingConditions(h[2])).toBe(false);
     });
   });
 
-  describe('minimum temp',function(){
-    it('cannot go below 10', function(){
-      for ( let i = 0; i < 10; i++ ) {
-        thermostat.down()
-      };
-      expect(thermostat.currentTemp).toBe(10);
-      thermostat.down();
-      expect(thermostat.currentTemp).toBe(10);
-    });
-  });
-
-  describe('power saving mode',function(){
-    it('default is on', function(){
-      expect(thermostat.powerSave).toBe(true);
+  describe('.countNeighbours', function(){
+    it('tallies one with one live neighbour', function() {
+      k = { loc: [ -1, -1 ], tally: 1, status: 0 }
+      board.alive([0,0])
+      board.countNeighbours()
+      expect(board.neighbTally[0]).toMatchObject(k);
     });
 
-    it('cannot go above 25 when on', function(){
-      for ( let i = 0; i < 5; i++ ) {
-        thermostat.up()
-      };
-      expect(thermostat.currentTemp).toBe(25);
-      thermostat.up();
-      expect(thermostat.currentTemp).toBe(25);
-    })
-
-
-    it('can be switched off', function(){
-      thermostat.switchSaveOff();
-      expect(thermostat.powerSave).toBe(false);
+    it('tallies two with two live neighbours', function() {
+      l = { loc: [ 0, -1 ], tally: 2, status: 0 }
+      board.alive([0,0])
+      board.alive([0,1])
+      board.countNeighbours()
+      expect(board.neighbTally[3]).toMatchObject(l);
     });
   })
 
-  describe('reset',function(){
-    it('resets to 20', function(){
-      thermostat.reset();
-      expect(thermostat.currentTemp).toBe(20)
-    });
-  });
-
-  describe('current energy usage',function(){
-    it('is medium on initialization', function(){
-      expect(thermostat.energyUsage()).toBe("medium-usage")
+  describe('.tick', function(){
+    let board = new Board
+    it('updates live with new locations', function() {
+      board.alive([0,0])
+      board.alive([0,1])
+      board.alive([1,0])
+      // console.log(board)
+      board.tick()
+      // console.log(board.neighbTally[0])
+      // expect(board.live).toContain([1,1])
     })
+  })
 
-    it('is low at 17', function() {
-      for ( let i = 0; i < 3; i++ ) {
-        thermostat.down();
-      };
-      expect(thermostat.energyUsage()).toBe("low-usage")
-    })
-
-    it('is high at 26 (PS must be off)', function() {
-      thermostat.switchSaveOff()
-      for ( let i = 0; i < 8; i++ ) {
-        thermostat.up()
-      };
-      expect(thermostat.energyUsage()).toBe("high-usage")
-    })
-  });
-});
+  // test('adds 1 + 2 to equal 3', () => {
+  //   skip
+  //   expect(sum(1, 2)).toBe(3);
+  // });
+  //
+  // test('two plus two is four', () => {
+  //   skip
+  //   expect(sum(2, 2)).not.toBe(5);
+  // });
+  //
+  // test('object assignment', () => {
+  //   const data = {one: 1};
+  //   data['two'] = 2;
+  //   expect(data).toEqual({one: 1, two: 2});
+  // });
+  // beforeEach(function() {
+  //   thermostat = new Thermostat();
+  // });
+  //
+  // describe('initialization', function(){
+  //   it('starts at temperature 20', function() {
+  //     expect(thermostat.currentTemp).toBe(20);
+  //   });
+  // });
+  //
+  // describe('up',function(){
+  //   it('can increase the temperature', function(){
+  //     thermostat.up();
+  //     expect(thermostat.currentTemp).toBe(21);
+  //   });
+  // });
+  //
+  // describe('down',function(){
+  //   it('can decrease the temperature', function(){
+  //     thermostat.down();
+  //     expect(thermostat.currentTemp).toBe(19);
+  //   });
+  // });
+  //
+  // describe('minimum temp',function(){
+  //   it('cannot go below 10', function(){
+  //     for ( let i = 0; i < 10; i++ ) {
+  //       thermostat.down()
+  //     };
+  //     expect(thermostat.currentTemp).toBe(10);
+  //     thermostat.down();
+  //     expect(thermostat.currentTemp).toBe(10);
+  //   });
+  // });
+  //
+  // describe('power saving mode',function(){
+  //   it('default is on', function(){
+  //     expect(thermostat.powerSave).toBe(true);
+  //   });
+  //
+  //   it('cannot go above 25 when on', function(){
+  //     for ( let i = 0; i < 5; i++ ) {
+  //       thermostat.up()
+  //     };
+  //     expect(thermostat.currentTemp).toBe(25);
+  //     thermostat.up();
+  //     expect(thermostat.currentTemp).toBe(25);
+  //   })
+  //
+  //
+  //   it('can be switched off', function(){
+  //     thermostat.switchSaveOff();
+  //     expect(thermostat.powerSave).toBe(false);
+  //   });
+  // })
+  //
+  // describe('reset',function(){
+  //   it('resets to 20', function(){
+  //     thermostat.reset();
+  //     expect(thermostat.currentTemp).toBe(20)
+  //   });
+  // });
+  //
+  // describe('current energy usage',function(){
+  //   it('is medium on initialization', function(){
+  //     expect(thermostat.energyUsage()).toBe("medium-usage")
+  //   })
+  //
+  //   it('is low at 17', function() {
+  //     for ( let i = 0; i < 3; i++ ) {
+  //       thermostat.down();
+  //     };
+  //     expect(thermostat.energyUsage()).toBe("low-usage")
+  //   })
+  //
+  //   it('is high at 26 (PS must be off)', function() {
+  //     thermostat.switchSaveOff()
+  //     for ( let i = 0; i < 8; i++ ) {
+  //       thermostat.up()
+  //     };
+  //     expect(thermostat.energyUsage()).toBe("high-usage")
+  //   })
+  // });
+})
--- src/gameOfLife.js
+++ src/gameOfLife.js
@@ -1,73 +1,86 @@
-function sum(a, b) {
-  return a + b;
-}
-module.exports = sum;
-# frozen_string_literal: true
 
-# Keeps track of and updates live cells
-
-class Board {
-
-  // attr_reader :dims, :live, :neighb_tally
-
-  const DEF_DIMS = 2
-  const NEIGHBOURS = [[-1, -1], [-1, 0], [-1, 1],
-                [0, -1], [0, 1],
-                [1, -1], [1, 0], [1, 1]]
-
-  constructor(dims = DEF_DIMS) {
-    this.dims = dims
+module.exports = class Board {
+  constructor() {
     this.live = []
+    this.NEIGHBOURS = [[-1, -1], [-1, 0], [-1, 1],
+                  [0, -1], [0, 1],
+                  [1, -1], [1, 0], [1, 1]]
   }
 
   alive(location) {
-    live.push(location)
+    this.live.push(location)
   }
 
   dead(location) {
-    live.delete(location)
+    this.live = this.live.filter(function(value, index, arr){
+        return JSON.stringify(value) !== JSON.stringify(location);
+    });
   }
 
-  tick() {
-    this.countNeighbours
-    this.updateLive
+  tick = () => {
+    console.log("inside tick")
+    this.countNeighbours()
+    this.updateLive()
   }
 
   countNeighbours() {
+    console.log("inside countNeighbours")
     this.neighbTally = []
-    this.live.each do |loc|
-      this.labelNeighbours(loc)
-    end
+    // var i;
+    for (var i=0; i < this.live.length; i++) {
+      this.labelNeighbours(this.live[i])
+    }
+    // return this.neighbTally
+    // this.live.each do |loc|
+    //   this.labelNeighbours(loc)
+    // end
   }
-
+  //
   labelNeighbours(loc) {
-    NEIGHBOURS.each do |nloc|
-      spot = [loc[0] + nloc[0], loc[1] + nloc[1]]
-      isInHash(spot) ? increaseCount(spot) : addToHash(spot)
-    end
+    console.log("inside labelNeighbours")
+    for (var i=0; i < this.NEIGHBOURS.length; i++) {
+      var spot = [loc[0] + this.NEIGHBOURS[i][0], loc[1] + this.NEIGHBOURS[i][1]]
+      this.isInHash(spot) ? this.increaseCount(spot) : this.addToHash(spot)
+    }
+    // NEIGHBOURS.each do |nloc|
+    //   spot = [loc[0] + nloc[0], loc[1] + nloc[1]]
+    //   isInHash(spot) ? increaseCount(spot) : addToHash(spot)
+    // end
   }
 
   isInHash(spot){
-    this.neighbTally.any? { |h| h[:location] == spot }
+    // this.neighbTally.any? { |h| h[:location] == spot }
+    return (this.neighbTally.map(o => JSON.stringify(Object.values(o['loc']))).includes(JSON.stringify(spot)))
   }
+  // h.map(o => JSON.stringify(Object.values(o['loc']))).includes(JSON.stringify(spot))
+
+  // match = (x) => JSON.stringify(x)
 
   increaseCount(spot) {
-    this.neighb_tally.select { |key| key[:location] == spot }[0][:tally] += 1
+    // this.neighb_tally.select { |key| key[:location] == spot }[0][:tally] += 1
+    for (var i=0; i<this.neighbTally.length; i++){
+      if (JSON.stringify(this.neighbTally[i]['loc']) === JSON.stringify(spot)){
+        this.neighbTally[i]['tally'] += 1
+      }
+    }
   }
 
   addToHash(spot) {
-    this.neighbTally.push( { location: spot,
+    this.neighbTally.push( { loc: spot,
                       tally: 1,
-                      status: live.include?(spot) ? 1 : 0 } )
+                      status: JSON.stringify(this.live).includes(JSON.stringify(spot)) ? 1 : 0 } )
   }
 
-  updateLive() {
+  updateLive = () => {
     this.live = []
-    this.neighbTally = this.neighbTally.select { |hash| this.livingConditions(hash) }
-    this.neighbTally.each { |hash| this.alive(hash[:location]) }
+    console.log(this.neighbTally)
+    this.neighbTally = this.neighbTally.filter(x => this.livingConditions(x));
+    for (var i=0; i<this.neighbTally.length; i++){
+      this.alive(this.neighbTally[i]['loc'])
+    }
   }
 
   livingConditions(hash) {
-    hash[:tally] == 3 || (hash[:tally] == 2 && hash[:status] == 1)
+    return (hash.tally === 3 || (hash.tally === 2 && hash.status === 1))
   }
 }
