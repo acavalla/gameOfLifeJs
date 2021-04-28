07c34c498ecec82c7f999b755c4ccf485fd0e23b
Anna Cavalla <Anna.Cavalla@gmail.com>
2021-04-28 23:09:59 +0100

    Added linter to package.json, deleted commented out code, light refactor

--- src/gameOfLife.js
+++ src/gameOfLife.js
@@ -12,55 +12,38 @@ module.exports = class Board {
   }
 
   dead(location) {
-    this.live = this.live.filter(function(value, index, arr){
+    this.live = this.live.filter(function(value) {
         return JSON.stringify(value) !== JSON.stringify(location);
     });
   }
 
   tick = () => {
-    console.log("inside tick")
     this.countNeighbours()
     this.updateLive()
   }
 
   countNeighbours() {
-    console.log("inside countNeighbours")
     this.neighbTally = []
-    // var i;
     for (var i=0; i < this.live.length; i++) {
       this.labelNeighbours(this.live[i])
     }
-    // return this.neighbTally
-    // this.live.each do |loc|
-    //   this.labelNeighbours(loc)
-    // end
   }
-  //
+
   labelNeighbours(loc) {
-    console.log("inside labelNeighbours")
     for (var i=0; i < this.NEIGHBOURS.length; i++) {
       var spot = [loc[0] + this.NEIGHBOURS[i][0], loc[1] + this.NEIGHBOURS[i][1]]
       this.isInHash(spot) ? this.increaseCount(spot) : this.addToHash(spot)
     }
-    // NEIGHBOURS.each do |nloc|
-    //   spot = [loc[0] + nloc[0], loc[1] + nloc[1]]
-    //   isInHash(spot) ? increaseCount(spot) : addToHash(spot)
-    // end
   }
 
   isInHash(spot){
-    // this.neighbTally.any? { |h| h[:location] == spot }
     return (this.neighbTally.map(o => JSON.stringify(Object.values(o['loc']))).includes(JSON.stringify(spot)))
   }
-  // h.map(o => JSON.stringify(Object.values(o['loc']))).includes(JSON.stringify(spot))
-
-  // match = (x) => JSON.stringify(x)
 
   increaseCount(spot) {
-    // this.neighb_tally.select { |key| key[:location] == spot }[0][:tally] += 1
     for (var i=0; i<this.neighbTally.length; i++){
-      if (JSON.stringify(this.neighbTally[i]['loc']) === JSON.stringify(spot)){
-        this.neighbTally[i]['tally'] += 1
+      if (JSON.stringify(this.neighbTally[i].loc) === JSON.stringify(spot)){
+        this.neighbTally[i].tally += 1
       }
     }
   }
@@ -68,15 +51,14 @@ module.exports = class Board {
   addToHash(spot) {
     this.neighbTally.push( { loc: spot,
                       tally: 1,
-                      status: JSON.stringify(this.live).includes(JSON.stringify(spot)) ? 1 : 0 } )
+                      status: JSON.stringify(this.live).includes(spot) ? 1 : 0 } )
   }
 
   updateLive = () => {
     this.live = []
-    console.log(this.neighbTally)
     this.neighbTally = this.neighbTally.filter(x => this.livingConditions(x));
     for (var i=0; i<this.neighbTally.length; i++){
-      this.alive(this.neighbTally[i]['loc'])
+      this.alive(this.neighbTally[i].loc)
     }
   }
 
